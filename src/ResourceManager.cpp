62f63dfee4f19afb45a1bb24e9eb99ee07d24378
Timothy Hahn <timothyahahn@gmail.com>
2019-04-28 06:55:18 -0600

    Fix Octave Stuff #11

--- src/ResourceManager.cpp
+++ src/ResourceManager.cpp
@@ -61,8 +61,7 @@ ResourceManager::ResourceManager()
                                        threadZero->getNumberOfWavelengths()]) {
   calc_min_spans();
 
-  //OctaveWrapper::build_nonlinear_datastructure(sys_fs, sys_link_xpm_database);
-  OctaveWrapper::helloWorld();
+  OctaveWrapper::build_nonlinear_datastructure(sys_fs, sys_link_xpm_database);
 
   precompute_fwm_combinations();
 }
